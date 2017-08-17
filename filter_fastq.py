a3a18386b75a32b079f4582dcd532a18b6d8b801
Angel Mojarro <angel.mojarro@gmail.com>
2017-08-17 19:01:36 -0400

    Update filter_fastq.py

--- filter_fastq.py
+++ filter_fastq.py
@@ -1,32 +1,40 @@
+# Modified by AM for CarrierSeq application
+
 from Bio import SeqIO
-import os
 import math
+from Tkinter import Tk
+import sys
 
-name = raw_input('path to file: ')
-min =float(raw_input('enter minimum length: '))
-max =float(raw_input('enter maximum length: '))
-qs = float(raw_input('Enter quality score: '))
+name = sys.argv[1]
+qs = float(sys.argv[3])
+output = sys.argv[2]
 
-qual_sequences = [] # Setup an empty list
+count = 0
+for rec in SeqIO.parse(name, "fastq"):
+    count += 1
+
+qual_sequences = []
+
 cnt = 0
-count= 0
+for rec in SeqIO.parse(name, "fastq"):
+    rec.letter_annotations["phred_quality"]
+    probs = []
+    for q in rec.letter_annotations["phred_quality"]:
+        e = float(math.pow(10.0,-1*(float(q)/10.0)))
+#        print q, e
+        probs.append(e)
+    av_prob = float(sum(probs))/float(len((rec.letter_annotations["phred_quality"])))
+#    print av_prob
+    av_q = float(-10.0*(math.log10(float(av_prob))))
+ #   print av_prob, av_q
+    if av_q >= qs:
+	cnt += 1
+        qual_sequences.append(rec)
 
-with open('filtered.fq', 'w') as output_handle:
-	for rec in SeqIO.parse(open(name), "fastq") :
-		count += 1
+output_handle = open(output +'.fa', "w")
+SeqIO.write(qual_sequences, output_handle, "fasta")
+output_handle.close()
 
-		rec.letter_annotations["phred_quality"]
-		probs = []
-		for q in rec.letter_annotations["phred_quality"]:
-			e = float(math.pow(10.0,-1*(float(q)/10.0)))
-			probs.append(e)
-		av_prob = float(sum(probs))/float(len((rec.letter_annotations["phred_quality"])))
-		av_q = float(-10.0*(math.log10(float(av_prob))))
-		
-		if len(rec.seq) >=min and len(rec.seq) <=max and av_q >= qs:
-			cnt += 1
-			# Add this record to our list
-			SeqIO.write(rec, output_handle, "fastq")
-		print '  Reads processed  ', count ,  ' \r',
-			
-print cnt,'filtered reads saved'
+output_handle = open(output +'.fq', "w")
+SeqIO.write(qual_sequences, output_handle, "fastq")
+output_handle.close()
